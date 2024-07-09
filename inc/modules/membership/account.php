1ccec0578c4a74c1f3883a86afc4c28212862815
WebDevs.pro <shock_31337@ukr.net>
2024-07-09 21:19:28 +0300

    1.7

--- inc/modules/membership/account.php
+++ inc/modules/membership/account.php
@@ -8,6 +8,9 @@ class TFC_User_Account {
 
 	public function __construct() {
 		add_action( 'wp_login', array( $this, 'redirect_subscribers_to_account' ), 10, 2 );
+      add_filter( 'show_admin_bar', array( $this, 'disable_admin_bar_for_subscribers' ) );
+		add_action( 'admin_init', array( $this, 'redirect_subscribers_from_admin' ) );
+
       add_shortcode( 'tfc_account', array( $this, 'user_account_shortcode' ) );
 	}
 
@@ -15,6 +18,20 @@ class TFC_User_Account {
       error_log( "user\n" . print_r( $user, true ) . "\n" );
 
 		if ( in_array( 'subscriber', (array) $user->roles, true ) ) {
+			wp_redirect( home_url( '/account' ) );
+			exit;
+		}
+	}
+
+   public function disable_admin_bar_for_subscribers( $show_admin_bar ) {
+		if ( current_user_can( 'subscriber' ) ) {
+			return false;
+		}
+		return $show_admin_bar;
+	}
+
+	public function redirect_subscribers_from_admin() {
+		if ( current_user_can( 'subscriber' ) && is_admin() ) {
 			wp_redirect( home_url( '/account' ) );
 			exit;
 		}
