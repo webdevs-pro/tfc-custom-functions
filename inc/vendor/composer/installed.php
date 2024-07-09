7d846bdf14c44b03681d6bbef36ae80ceba209db
WebDevs.pro <shock_31337@ukr.net>
2024-07-09 17:51:30 +0300

    1.0

---
+++ inc/vendor/composer/installed.php
@@ -0,0 +1,34 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '69bd835facddd0a513366a15a2bc02ce66a47b6a',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '69bd835facddd0a513366a15a2bc02ce66a47b6a',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'yahnis-elsts/plugin-update-checker' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => 'dfc396c0110fa1739dd4e349a1889d587ac21644',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../yahnis-elsts/plugin-update-checker',
+            'aliases' => array(
+                0 => '9999999-dev',
+            ),
+            'dev_requirement' => false,
+        ),
+    ),
+);
---
+++ tfc-custom-functions.php
@@ -0,0 +1,65 @@
+<?php
+/**
+ * Plugin Name: Toms Flight Club Custom Functions
+ * Plugin URI: https://github.com/webdevs-pro/tfc-custom-functions
+ * Version: 1.0
+ * Description: A place for custom functions for tomsflightclub.com website
+ * Author: Alex Ishchenko
+ * Author URI: https://website.cv.ua
+ */
+
+use YahnisElsts\PluginUpdateChecker\v5\PucFactory;
+
+final class TFC_Plugin {
+
+	public function __construct() {
+		$this->define_constants();
+		$this->include_files();
+		$this->init_plugin_update_checker();
+	}
+
+	function define_constants() {
+		if ( ! function_exists( 'get_plugin_data' ) ) {
+			require_once ( ABSPATH . 'wp-admin/includes/plugin.php' );
+		}
+		if ( ! function_exists( 'get_home_path' ) ) {
+			require_once ( ABSPATH . 'wp-admin/includes/file.php' );
+		}
+		define( 'TFC_PLUGIN_VERSION', get_plugin_data( __FILE__ )['Version'] );
+		// define( 'TFC_HOME_PATH', get_home_path() );
+		define( 'TFC_HOME_PATH', ABSPATH );
+		define( 'TFC_PLUGIN_BASENAME', plugin_basename( __FILE__ ) );
+		define( 'TFC_PLUGIN_DIR', dirname( __FILE__ ) );
+		define( 'TFC_PLUGIN_FILE', __FILE__ );
+		define( 'TFC_PLUGIN_DIR_URL', plugin_dir_url( __FILE__ ) );
+	}
+
+	function include_files() {
+		require_once ( TFC_PLUGIN_DIR . '/inc/vendor/autoload.php' );
+		require_once ( TFC_PLUGIN_DIR . '/inc/plugin.php' );
+	}
+
+	function init_plugin_update_checker() {
+		$UpdateChecker = PucFactory::buildUpdateChecker(
+			'https://github.com/webdevs-pro/customcaddie-custom-functions',
+			__FILE__,
+			'tfc-custom-functions'
+		);
+		
+		//Set the branch that contains the stable release.
+		$UpdateChecker->setBranch( 'main' );
+	}
+
+}
+
+new TFC_Plugin();
+
+
+
+add_filter( 'hello_elementor_viewport_content', function( $viewport_content ) {
+	return 'width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no';
+} );
+
+add_filter( 'elementor/template/viewport_tag', function( $meta_tag, $context ) {
+	return '<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">';
+}, 10, 2 );
