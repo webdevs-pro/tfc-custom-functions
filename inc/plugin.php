9bfb9d3a466daec873afb502b7c6f10651da4b56
WebDevs.pro <shock_31337@ukr.net>
2024-07-10 02:16:38 +0300

    save

--- inc/plugin.php
+++ inc/plugin.php
@@ -11,18 +11,15 @@ require_once ( TFC_PLUGIN_DIR . '/inc/modules/membership/account.php' );
 
 /**
  * Disable admin password change notification for subscriber user role.
- *
- * @param bool  $send   Whether to send the password change email.
- * @param array $user   User object.
- * @param string $notify Type of notification. Can be 'user', 'admin', or 'both'.
- * @return bool False if the email should not be sent, true otherwise.
  */
-add_filter( 'send_password_change_email', 'disable_admin_password_change_notification', 10, 3 );
-
-function disable_admin_password_change_notification( $send, $user, $notify ) {
-	// Check if the notification is for admin and the user role is subscriber.
-	if ( 'admin' === $notify && in_array( 'subscriber', (array) $user->roles ) ) {
-		return false;
+if ( ! function_exists( 'wp_password_change_notification' ) ) {
+	function wp_password_change_notification( $user ) {
+		// Get the user object to check the role.
+		$user_obj = get_userdata( $user->ID );
+		
+		// Check if the user's role is subscriber.
+		if ( in_array( 'subscriber', ( array ) $user_obj->roles ) ) {
+			return false;
+		}
 	}
-	return $send;
 }
