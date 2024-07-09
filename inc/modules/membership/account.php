1553ea6ab08891cf3f1690a8cf1f9016a18a6b2a
WebDevs.pro <shock_31337@ukr.net>
2024-07-09 21:27:55 +0300

    1.8

--- inc/modules/membership/account.php
+++ inc/modules/membership/account.php
@@ -10,32 +10,49 @@ class TFC_User_Account {
 		add_action( 'wp_login', array( $this, 'redirect_subscribers_to_account' ), 10, 2 );
       add_filter( 'show_admin_bar', array( $this, 'disable_admin_bar_for_subscribers' ) );
 		add_action( 'admin_init', array( $this, 'redirect_subscribers_from_admin' ) );
+      add_action( 'template_redirect', array( $this, 'redirect_non_logged_users_from_account' ) );
+
 
       add_shortcode( 'tfc_account', array( $this, 'user_account_shortcode' ) );
 	}
 
-	public function redirect_subscribers_to_account( $user_login, $user ) {
-      error_log( "user\n" . print_r( $user, true ) . "\n" );
-
-		if ( in_array( 'subscriber', (array) $user->roles, true ) ) {
-			wp_redirect( home_url( '/account' ) );
-			exit;
-		}
-	}
+   public function redirect_subscribers_to_account( $user_login, $user ) {
+      if ( in_array( 'subscriber', (array) $user->roles, true ) ) {
+         wp_redirect( home_url( '/account' ) );
+         exit;
+      }
+   }
 
    public function disable_admin_bar_for_subscribers( $show_admin_bar ) {
-		if ( current_user_can( 'subscriber' ) ) {
-			return false;
-		}
-		return $show_admin_bar;
-	}
+      if ( current_user_can( 'subscriber' ) ) {
+         return false;
+      }
+      return $show_admin_bar;
+   }
 
-	public function redirect_subscribers_from_admin() {
-		if ( current_user_can( 'subscriber' ) && is_admin() ) {
-			wp_redirect( home_url( '/account' ) );
-			exit;
-		}
-	}
+   public function redirect_subscribers_from_admin() {
+      if ( current_user_can( 'subscriber' ) && is_admin() ) {
+         wp_redirect( home_url( '/account' ) );
+         exit;
+      }
+   }
+
+   public function redirect_non_logged_users_from_account() {
+      if ( ! is_user_logged_in() && is_page( 'account' ) ) {
+         wp_redirect( home_url( '/' ) );
+         exit;
+      }
+   }
+
+
+
+
+
+
+
+
+
+
 
 	public function user_account_shortcode() {
       ob_start();
