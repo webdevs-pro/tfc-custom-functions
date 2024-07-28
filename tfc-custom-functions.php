f0d464b997f0d2d5a3091c239e67aa6c778a53ab
WebDevs.pro <shock_31337@ukr.net>
2024-07-28 13:06:58 +0300

    1.33

--- tfc-custom-functions.php
+++ tfc-custom-functions.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: Toms Flight Club Custom Functions
  * Plugin URI: https://github.com/webdevs-pro/tfc-custom-functions
- * Version: 1.32
+ * Version: 1.33
  * Description: A place for custom functions for tomsflightclub.com website
  * Author: Alex Ishchenko
  * Author URI: https://website.cv.ua
@@ -63,3 +63,65 @@ add_filter( 'hello_elementor_viewport_content', function( $viewport_content ) {
 add_filter( 'elementor/template/viewport_tag', function( $meta_tag, $context ) {
 	return '<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">';
 }, 10, 2 );
+
+
+
+/**
+ * Delete 'deal' posts older than a week along with their attachments.
+ */
+function tfc_delete_old_deal_posts() {
+	// Calculate the date a week ago from today.
+	$date_query = date( 'Y-m-d H:i:s', strtotime( '-1 week' ) );
+
+	// Query for posts older than a week.
+	$query = new WP_Query(
+		array(
+			'post_type'      => 'deal',
+			'posts_per_page' => -1,
+			'date_query'     => array(
+					array(
+						'before' => $date_query,
+					),
+			),
+			'fields'         => 'ids', // Only get post IDs.
+		)
+	);
+
+	// Loop through the post IDs and delete them along with their attachments.
+	foreach ( $query->posts as $post_id ) {
+		// Get the attachments for the post.
+		$attachments = get_attached_media( '', $post_id );
+
+		// Delete each attachment.
+		foreach ( $attachments as $attachment ) {
+			wp_delete_attachment( $attachment->ID, true );
+		}
+
+		// Delete the post.
+		wp_delete_post( $post_id, true ); // true to force delete without moving to trash.
+	}
+}
+
+/**
+* Schedule the daily event to delete old deal posts if not already scheduled.
+*/
+function tfc_schedule_daily_delete_old_deal_posts() {
+	if ( ! wp_next_scheduled( 'tfc_daily_delete_old_deal_posts' ) ) {
+		wp_schedule_event( time(), 'daily', 'tfc_daily_delete_old_deal_posts' );
+	}
+}
+add_action( 'wp', 'tfc_schedule_daily_delete_old_deal_posts' );
+
+// Hook the delete function to the scheduled event.
+add_action( 'tfc_daily_delete_old_deal_posts', 'tfc_delete_old_deal_posts' );
+
+/**
+* Clear the scheduled event upon deactivation.
+*/
+function tfc_clear_scheduled_delete_old_deal_posts() {
+	$timestamp = wp_next_scheduled( 'tfc_daily_delete_old_deal_posts' );
+	if ( $timestamp ) {
+		wp_unschedule_event( $timestamp, 'tfc_daily_delete_old_deal_posts' );
+	}
+}
+register_deactivation_hook( __FILE__, 'tfc_clear_scheduled_delete_old_deal_posts' );
