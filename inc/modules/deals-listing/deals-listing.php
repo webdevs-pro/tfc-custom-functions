0d5edc1a975e0d3eb57ad0949df80d30a3d6cba9
WebDevs.pro <shock_31337@ukr.net>
2024-08-25 11:47:42 +0300

    Update deals-listing.php

--- inc/modules/deals-listing/deals-listing.php
+++ inc/modules/deals-listing/deals-listing.php
@@ -102,7 +102,7 @@ class TFC_Deals_Listing {
 	private function render_become_a_member_button( $post_id ) {
 		$this->render_deal_dates( $post_id );
 
-		echo '<a class="tfc-loop-subscribe" href="/subscribe-london" role="button">Become a Free Member</a>';
+		echo '<a class="tfc-loop-subscribe" href="/signup" role="button">Become a Free Member</a>';
 	}
 
 
