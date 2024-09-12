22889ec952d2aa8b5ddd171608da6e8086b44307
WebDevs.pro <shock_31337@ukr.net>
2024-09-12 09:51:42 +0300

    1.74

--- inc/modules/brevo/class-brevo-api.php
+++ inc/modules/brevo/class-brevo-api.php
@@ -124,7 +124,7 @@ class TFC_Brevo_API {
 			} while ( count( $contacts->getContacts() ) === $limit );
 
 			if ( empty( $filtered_emails ) ) {
-				$tfc_logger->log( "No contacts found for the specified criteria: City: {$origin_city}, Subscription: {$subscription_type}" . print_r( $log_data, true ) . "\n" );
+				$tfc_logger->log( "No contacts found for the specified criteria: City: {$origin_city}, Subscription: " . ( $subscription_type == 1 ? 'Paid' : ( $subscription_type == 2 ? 'Free' : 'Unknown' ) ) . "\n" );
 				return;
 			}
 
